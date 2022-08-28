a8744b63597b4bbe480e0bf4c2881e17b7de9b3a
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-28 15:18:35 +0200

    Add calculations for calculateDisselUsageForDistance and probabilityOfUnitInjectorFail

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,17 +1,30 @@
 exports.calculateDisselUsageForDistance = async (req, res, next) => {
     try {
-        res.json({message: 'CalculateDisselUsageForDistance Route'})
+        const { distance, yearOfProduction, fuelUsagePer100km} = req.query;
+        if(!distance || !yearOfProduction || !fuelUsagePer100km) {
+            res.status(500).json({message: 'Please make sure you have provided all of the following: distance, yearOfProduction and fuelUsagePer100km'});
+        } else if(isNaN(distance*1) || isNaN(yearOfProduction*1) || isNaN(fuelUsagePer100km*1)) {
+            res.status(500).json({message: 'Please make sure you have provided numbers for: distance, yearOfProduction and fuelUsagePer100km'});
+        } else {
+            const result = fuelUsagePer100km/100*distance;
+            res.status(200).json({message: `Dissel Usage For Distance is ${result}`});
+
+        }
     } catch (error) {
-        res.status(500);
-        res.json({message: error})
+        res.status(500).json({message: error})
     }
 }
 
 exports.probabilityOfUnitInjectorFail = async (req, res, next) => {
     try {
-        res.json({message: 'ProbabilityOfUnitInjectorFail Route'})
+        const { vin } = req.query;
+        const randomNumber = Math.floor(Math.random() * 101);
+        if(!vin) {
+            res.status(500).json({message: 'Please make sure you have provided vin'});
+        } else {
+            res.status(200).json({message: `Probability Of Unit Injector Fail is ${randomNumber/100}`});
+        }
     } catch (error) {
-        res.status(500);
-        res.json({message: error})
+        res.status(500).res.json({message: error})
     }
 }
