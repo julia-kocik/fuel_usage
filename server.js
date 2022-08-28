0b4d9cb48341813b12842a8fe2c629b26e2e9c2f
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-28 13:34:27 +0200

    Connect routes in server.js

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api', require('./routes/routes'));
+
 app.listen(5000, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port 5000`);
 })
